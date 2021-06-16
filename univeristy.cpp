471d699a2d642b6b6e948efdcbe9dddb2f668751
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-06-16 13:58:51 +0200

    Implemented searching functions

--- univeristy.cpp
+++ univeristy.cpp
@@ -8,26 +8,51 @@ University::University(const Student& s) {
 University::~University() {}
 
 void University::dispalayBase() {
-    for (int i = 0; i < university_.size() - 1; i++) {
-        std::cout << i << "." << university_[i].getName() << ", "
-                  << university_[i].getLname() << ", "
-                  << university_[i].getAdress() << ", "
-                  << university_[i].getIndex() << ", "
-                  << university_[i].getPesel() << ", "
-                  << university_[i].getGender() << '\n';
+    for (const auto& student : univercity_) {
+        std::cout << student -> getName() << ", "
+                  << student -> getSurname() << ", "
+                  << student -> getAdress() << ", "
+                  << student -> getIndex() << ", "
+                  << student -> getPesel() << ", "
+                  << student -> getGender() << '\n';
     }
 }
+
+Student* University::findBySurname(const std::string& surname){
+    auto isTheSame = [](Student* student){ return student -> getSurname() == surname; };
+    auto result = std::find_if(university_.begin(), university_.end(), isTheSame);
+
+    if (result == university.end()) {
+        std::cout << "There is no such student in our database with given surname" << '\n';
+        return nullptr;//nie wiem ??
+    }
+
+    return result;
+}
+
+Student* University::findByPesel(const long long& pesel){
+    auto isTheSame = [](Student* student){ return student -> getPesel() == pesel; };
+    auto result = std::find_if(university_.begin(), university_.end(), isTheSame);
+   
+    if (result == university.end()) {
+        std::cout << "There is no such student in our database with given PESEL number" << '\n';
+        return nullptr;//nie wiem co tutaj ??
+    }
+   
+    return result;
+}
+   
 // Implemented sort by pesel
 void University::sortByPesel() {
     std::sort(university_.begin(), university_.end(),
               [](const Student& lhsPtr, const Student& rhsPtr) {
-                  return lhsPtr.getPesel() < rhsPtr.getPesel();
+                  return lhsPtr -> getPesel() < rhsPtr -> getPesel();
               });
 }
 // Implemented sort by LName
 void University::sortbyLname() {
     std::sort(university_.begin(), university_.end(),
               [](const Student& lhsPtr, const Student& rhsPtr) {
-                  return lhsPtr.getLname() < rhsPtr.getLname();
+                  return lhsPtr -> getLname() < rhsPtr -> getLname();
               });
 }
